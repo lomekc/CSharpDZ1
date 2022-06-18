80cabf6ff92384636c739a701d94c915c8a7a250
lomekc <lomekc@mail.ru>
2022-06-18 14:04:11 +0300

    1st DZ C#

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿/*
+// задача 2
+int a,b; 
+
+Console.WriteLine("Input First Number ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input Second Number ");
+b = Convert.ToInt32(Console.ReadLine());
+
+if ( a > b)
+{
+    Console.WriteLine("max number" + " " + a);
+}
+else
+{
+    Console.WriteLine("max number" + " " + b);
+}
+*/
+
+/*
+// задача 4
+
+int a,b,c, max; 
+
+Console.WriteLine("Input First Number ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input Second Number ");
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input Third Number ");
+c = Convert.ToInt32(Console.ReadLine());
+
+{
+  max = a;
+if (b > a)
+    max = b;
+if (c > max)
+    max = c;
+Console.WriteLine("max number" + " " + max);
+}
+
+*/
+
+/*
+//задача 6
+
+int a; 
+
+Console.WriteLine("Input Number ");
+a = Convert.ToInt32(Console.ReadLine());
+
+
+if ( a % 2 == 0)
+{
+    Console.Write("Yes");
+}
+
+else 
+{
+    Console.Write("No");
+}
+
+*/
+
+/*
+// задача 8
+
+int a, b; 
+
+Console.WriteLine("Input Number ");
+a = Convert.ToInt32(Console.ReadLine());
+
+b = 1;
+
+while (b <= a)
+
+{
+    if (b%2==0)
+    Console.Write(b);
+    b++;
+}
+*/
